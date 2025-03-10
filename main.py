f64cb0e449627c84108920a4094dd5b476253351
haewonwon <khcw1029@daum.net>
2025-03-10 19:07:47 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# engScore = 90
+# mathScore = 35
+# korScore = 88
+# scoreSum = engScore + mathScore + korScore
+# scoreAverge = scoreSum / 3
+# print("세 과목의 평균은", scoreAverge, "점입니다.")
+
+# pi = 3.14
+# radius = 5
+# height = 10
+# volume = pi * radius**2 * height
+# print(volume)
